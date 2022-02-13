8286e8ee1f88984033c23947cfab726b7457abde
mwoedlinger <matthias.woedlinger@gmail.com>
2022-02-13 12:56:59 +0100

    document encode.py

--- encode.py
+++ encode.py
@@ -3,64 +3,53 @@ import numpy as np
 import argparse
 
 
-def integer_to_bit_list(integer, num_bytes=1):
+def integer_to_bit_list(integer: int, num_bytes: int=1) -> list:
+    """Convert an integer to a list of bits
+
+    Args:
+        integer (int): input integer
+        num_bytes (int, optional): byte number of the integer. 1 corresponds to a C char value. Defaults to 1.
+
+    Returns:
+        list: list of bit values
+    """
     return [integer >> i & 1 for i in range(num_bytes*8-1, -1, -1)]
 
-def encode_img(img, message, bit_idx=0):
+def encode_img(img: np.array, message: bytes, bit_idx: int=0) -> np.array:
+    """Hide message in img
+
+    Args:
+        img (np.array): Image as numpy array
+        message (bytes): message as byte string
+        bit_idx (int, optional): Which bit to hide the message. If set to zero the least important bit is used. 
+                                 Defaults to 0.
+
+    Returns:
+        np.array: image with hidden message as numpy array
+    """
     width, height, channels = img.shape
-    assert width*height*channels >= 8*len(message) + 24    
+    assert width*height*channels >= 8*len(message) + 24, "Message is too long."
     
-    # The first 24 bits encode the length of the message (2^24 is larger than the number of pixels in a 4k image)
+    # The first 24 bits encode the length of the message
     bitstream = np.concatenate([integer_to_bit_list(b) for b in message]).astype(np.uint8)
     message_len = len(bitstream)
-    bitstream_metainfo = integer_to_bit_list(message_len, num_bytes=3)
 
+    # Create list of bits containing message len + the message itself
+    bitstream_metainfo = integer_to_bit_list(message_len, num_bytes=3)
     img_array_1d = img.reshape(height*width*channels)
     bit_array_1d = np.array([int(b) for b in bitstream_metainfo] + 
                             [int(b) for b in bitstream])
     
-    # pad the bit array with the smallest bits from the remaining pixels in the image such that the length matches the length of the flattened image
+    # pad the bit array with the smallest bits with random bits such that the length matches the length of the flattened image
     bit_array_1d_padded = np.concatenate([bit_array_1d, 
                                           np.random.randint(0, 2, len(img_array_1d)-len(bit_array_1d))])
     bit_array_2d_padded = bit_array_1d_padded.reshape(img.shape)
     
+    # Zero out the image bits that will be used to store the message
     bit_window = (np.ones(img.shape)*255 - (1<<bit_idx)).astype(np.uint8)
-    
+
     return (img & bit_window) | (bit_array_2d_padded.astype('uint8')<<bit_idx)
 
-# def pad_zeros(l, pad_to_length=8):
-#     return '0'*(pad_to_length - len(l)) + l
-
-# def char_to_bit_str(c):
-#     return pad_zeros(bin(ord(c)).split('b')[1])
-
-# def char_list_to_bitstream(cl):
-#     cl_bins = [char_to_bit_str(c) for c in cl]
-#     return ''.join(cl_bins)
-
-# def encode_img(img, message, bit_idx=0):
-#     message_char_list = [m for m in message]
-#     width, height, channels = img.shape
-#     assert width*height*channels >= 8*len(message_char_list) + 24
-    
-#     bitstream = char_list_to_bitstream(message_char_list)
-    
-#     # The first 24 bits encode the length of the message (2^24 is larger than the number of pixels in a 4k image)
-#     message_len = len(bitstream)
-#     bitstream_metainfo = pad_zeros(bin(message_len).split('b')[1], pad_to_length=24)
-
-#     img_array_1d = img.reshape(height*width*channels)
-#     bit_array_1d = np.array([int(b) for b in bitstream_metainfo] + 
-#                             [int(b) for b in bitstream])
-    
-#     # pad the bit array with the smallest bits from the remaining pixels in the image such that the length matches the length of the flattened image
-#     bit_array_1d_padded = np.concatenate([bit_array_1d, 
-#                                           np.random.randint(0, 2, len(img_array_1d)-len(bit_array_1d))]) 
-#     bit_array_2d_padded = bit_array_1d_padded.reshape(img.shape)
-    
-#     bit_window = (np.ones(img.shape)*255 - (1<<bit_idx)).astype(np.uint8)
-    
-#     return (img & bit_window) | (bit_array_2d_padded.astype('uint8')<<bit_idx)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Hide a file in an image.')
